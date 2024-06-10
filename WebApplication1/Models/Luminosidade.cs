f1177614c7a04d9f5ef6718d00cff100208a7d62
pedro arthur visconti <pedroarthurvisconti@hotmail.com>
2024-06-09 23:05:41 -0300

    Criação das classes temperatura e luminosidade

---
+++ WebApplication1/Models/Luminosidade.cs
@@ -0,0 +1,13 @@
+﻿namespace ESP32.Models
+{
+    public class Luminosidade
+    {
+        int id_luminosidade {  get; set; }
+        int id_dispositivo { get; set; }
+        int luminosidade { get; set; }
+        DateTime data {  get; set; }
+
+
+
+    }
+}
---
+++ WebApplication1/Models/Temperatura.cs
@@ -0,0 +1,10 @@
+﻿namespace ESP32.Models
+{
+    public class Temperatura
+    {
+        int id_temperatura {  get; set; }
+        int id_dispositivo { get; set; }
+        float temperatura { get; set; }
+        DateTime data {  get; set; }
+    }
+}
